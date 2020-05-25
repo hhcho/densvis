a16c3b63e1aab5352d180656bfeb6e156011f5b8
Ashwin Narayan <ashwinn@mit.edu>
2020-05-24 22:19:51 -0400

    First-order R compatibility

---
+++ densmap/densmap.R
@@ -0,0 +1,58 @@
+if(!require("reticulate")) install.packages("reticulate")
+if(!require("optparse")) install.packages("optparse")
+library("reticulate")
+# install.packages("optparse")
+library("optparse")
+
+densMAP <- function(data, n_neighbors, n_epochs, ndim, dens_frac, dens_lambda, final_dens, var_shift, metric, min_dist,verbose) { 
+    if(missing(n_neighbors)) { 
+        n_neighbors <- 30
+    }
+    if(missing(n_epochs)) { 
+        n_epochs <- 750
+    }
+    if(missing(ndim)) { 
+        ndim <- 2
+    }
+    if(missing(dens_frac)) { 
+        dens_frac <- .3
+    }
+    if(missing(dens_lambda)) { 
+        dens_lambda <- 2.
+    }
+    if(missing(final_dens)) { 
+        final_dens <- TRUE
+    }
+    if(missing(var_shift)) { 
+        var_shift <- .1
+    }
+    if(missing(metric)) { 
+        metric <- "euclidean"
+    }
+    if(missing(min_dist)) { 
+        min_dist <- .1
+    }
+    if(missing(verbose)) { 
+        verbose <- TRUE
+    }
+            
+    dm <- reticulate::import('densmap')
+    
+    densMAP <- dm$densMAP("n_neighbors"=as.integer(n_neighbors),"n_epochs"=as.integer(n_epochs), 
+                          "n_components"=as.integer(ndim), "dens_frac"=dens_frac, "dens_lambda"=dens_lambda, 
+                          "final_dens"=final_dens, "var_shift"=var_shift, "metric"=metric,
+                          "min_dist"=min_dist, "verbose"=verbose)
+    
+    mdm.full <- densMAP$fit_transform(data)
+        
+    return(mdm.full)
+}
+
+#dm <- reticulate::import('densmap')
+
+#data <- dm$load_trial$load_trial()
+#densMAP <- dm$densMAP("final_dens"=FALSE)
+
+#mdm.full <- densMAP$fit_transform(data)
+
+#mdm.full
